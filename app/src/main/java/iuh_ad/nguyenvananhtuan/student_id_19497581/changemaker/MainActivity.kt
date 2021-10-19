fa810bbf315e5a892d51eb459ae92726ea3e4279
tuanio <nvatuan3@gmail.com>
2021-10-19 16:05:48 +0700

    OK

---
+++ app/src/main/java/iuh_ad/nguyenvananhtuan/student_id_19497581/changemaker/MainActivity.kt
@@ -0,0 +1,11 @@
+package iuh_ad.nguyenvananhtuan.student_id_19497581.changemaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
